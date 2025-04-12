a778d85c02da169ef6ae99014043244d1e68f5c7
Fred <leferpz@gmail.com>
2025-04-12 15:40:36 -0400

    Added skeleton placeholders while users wait for posts to load, and a login prompt if not logged in

--- front-end-user/src/pages/HomePage.jsx
+++ front-end-user/src/pages/HomePage.jsx
@@ -1,10 +1,11 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { Box, Text, Grid, GridItem, Flex, Heading, HStack, Button, Separator } from '@chakra-ui/react';
+import { Box, Text, Grid, GridItem, Flex, Heading, HStack, Button, Skeleton, Separator, StackSeparator } from '@chakra-ui/react';
 import { Link, useNavigate, useParams } from "react-router-dom"
 import LoginPage from "./LoginPage";
 
 function HomePage() {
+    const [isLoaded, setIsLoaded] = useState(false);
     const [posts, setPosts] = useState([])
     const [token, setToken] = useState(localStorage.token);
     const navigate = useNavigate();
@@ -18,33 +19,49 @@ function HomePage() {
                 }
             });
             const postsData = postsList.data
-            setPosts(postsData)
+            setPosts(postsData);
+            setIsLoaded(true);
         }
 
     useEffect(() => {
         postsGet()
             }
-    ,[[], localStorage.token])
+    ,[])
 
     if (localStorage.token) {
     return (
         <section>
+            <StackSeparator h="20px"></StackSeparator>
+            <Box p={10}>
             <Grid templateColumns="repeat(3, 1fr)" gap="6">
-                {posts.map(post => 
+                {isLoaded ? (
+                posts.map(post => 
                     <Link to={`/posts/${post.id}`}>
-                        <GridItem backgroundColor="blackAlpha.800" key={post.id} p={10} variant="surface" borderRadius="md">
-                            <Text>{post.title}</Text>
-                            <Separator h="20px"></Separator>
+                        <GridItem _hover={{bg: "blue.800", transform: "scale(1.02)"}} backgroundColor="blackAlpha.800" key={post.id} p={7} variant="surface" borderRadius="md">
+                            <Text fontWeight="bold" fontSize="xl">{post.title}</Text>
+                            <Separator h="15px"></Separator>
                             <Text>{post.content}</Text>
                         </GridItem>
                     </Link>
-                )}
+                    )) : [1, 2, 3, 4, 5, 6].map((index) => (
+                        <GridItem key={index}>
+                          <Skeleton bg="blue.950" height="125px"/>
+                        </GridItem>
+                      ))};
             </Grid>
+            </Box>
         </section>
     )}
 
     else {
-        navigate("/login")
+        return (
+            <Flex height="80%" width="100%" justifyContent="center" alignItems="center">
+                <Box bg="blackAlpha.950" p={8} borderRadius="md">
+                    <Text>It looks like you're not logged in...</Text>
+                    <Link to="/login"><Text fontSize="xl" color="blue.300">Click here to login.</Text></Link>
+                </Box>
+            </Flex>
+        )
     }
 }
 
